078f18b7194c19a0836c0cb4e53b1f1d5ac62c64
Delaney Boyle <104108082+dnboyle20@users.noreply.github.com>
2022-04-20 21:12:58 -0400

    Update graytocolor.m

--- graytocolor.m
+++ graytocolor.m
@@ -1,3 +1,13 @@
+% BME 3053C Final Project
+% Author: Lauren Ellis
+% Course: BME 3053C Computer Applications for BME 
+% Group Members: Sarah Stenberg, Karley Baringer, and Delaney Boyle
+% Term: Spring 2022 
+% J. Crayton Pruitt Family Department of Biomedical Engineering 
+% University of Florida 
+% Email: lauren.ellis@ufl.edu 
+% April 18, 2022
+
 function rgbImage = gray2rgb(I)
 cmap = jet(256); % Or whatever one you want.
 rgbImage = ind2rgb(I, cmap);
